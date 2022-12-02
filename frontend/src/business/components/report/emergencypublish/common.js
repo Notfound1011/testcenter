57642b7346b006052ce55d2ea4c55feccf4f5181
Pauri <planes.prosody-0i@icloud.com>
2022-12-02 16:00:49 +0800

    update em_publish

--- frontend/src/business/components/report/emergencypublish/common.js
+++ frontend/src/business/components/report/emergencypublish/common.js
@@ -1,73 +1,33 @@
-import { Message } from 'element-ui'
+import { Message, Notification } from 'element-ui'
 
-export function formRules () {
-  return {
-    publish_type: [{
-      required: true,
-      type: 'array',
-      message: '请至少选择一个紧急发布类型',
-      trigger: 'change'
-    }],
-    publish_service: [{
-      required: true,
-      type: 'array',
-      message: '请至少选择一个相关变更服务',
-      trigger: 'change'
-    }],
-    jira_link: [{
-      required: false,
-      message: '请输入JIRA链接相关JIRA链接',
-      trigger: 'blur'
-    }],
-    config_pr: [{
-      required: false,
-      message: '请输入JIRA链接newconfig PR 链接',
-      trigger: 'blur'
-    }],
-    change_audit_link: [{
-      required: false,
-      message: '请输入JIRA链接change_audit 链接',
-      trigger: 'blur'
-    }],
-    confluence_link: [{
-      required: false,
-      message: '请输入confluence链接',
-      trigger: 'blur'
-    }],
-    publish_reason: [{
-      required: true,
-      message: '请输入紧急变更概述',
-      trigger: 'blur'
-    }],
-    developer: {users: [{
-        required: true,
-        type: 'array',
-        message: '请至少选择一个相关开发人员',
-        trigger: 'change'
-      }]},
-    reviewer: {users: [{
-        required: true,
-        type: 'array',
-        message: '请至少选择一个开发review人员',
-        trigger: 'change'
-      }]},
-    rollback_action: [{
-      required: true,
-      type: 'array',
-      message: '请至少选择一个回滚策略',
-      trigger: 'change'
-    }],
-    publish_time: [{
-      required: true,
-      message: '请选择预计变更时间',
-      trigger: 'change'
-    }],
-    executor: {users: [{
-        required: true,
-        type: 'array',
-        message: '请至少选择一个测试执行人',
-        trigger: 'change'
-      }]},
+// 消息通知
+export function notificationTips (type, title, message) {
+  // type : success | warning | error | info
+  switch (type) {
+    case 'success':
+    case 'warning':
+      Notification({
+        title: title,
+        message: message,
+        type: type,
+        position: 'top-left',
+        showClose: false
+      })
+      break
+    case 'info':
+      Notification.info({
+        title: title,
+        message: message,
+        position: 'top-left'
+      })
+      break
+    case 'error':
+      Notification.error({
+        title: title,
+        message: message,
+        position: 'top-left'
+      })
+      break
   }
 }
 
