071b679107d7ce604b207ce9983a63c85f621172
Pauri <planes.prosody-0i@icloud.com>
2022-12-04 13:06:59 +0800

    done dev

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -26,6 +26,13 @@ module.exports = {
           '^/api/tc/naguri': '/'
         }
       },
+      // '/api/tc/naguri': {
+      //   target: "http://3.1.250.199:7001",
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '^/api/tc/naguri': '/'
+      //   }
+      // },
       '/api/tc/pyServer': {
         target: "http://3.1.250.199:6200",
         changeOrigin: true,
