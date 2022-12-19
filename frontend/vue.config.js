50c325c5aae93a901d97660e9aaea30d57c0c416
Pauri <planes.prosody-0i@icloud.com>
2022-12-19 16:36:12 +0800

    update table style

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -20,8 +20,8 @@ module.exports = {
       //   ws: true,
       // },
       '/api/tc/naguri': {
-        // target: "http://3.1.250.199:7001",
-        target: "http://0.0.0.0:7001",
+        target: "http://3.1.250.199:7001",
+        // target: "http://0.0.0.0:7001",
         changeOrigin: true,
         pathRewrite: {
           '^/api/tc/naguri': '/'
