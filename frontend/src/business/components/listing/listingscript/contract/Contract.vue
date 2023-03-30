3e3313b4bb7c33e2a8e4f6f91bb44f550ec89f6d
yuyu.shi <yuyu.shi@cmexpro.com>
2023-03-30 18:53:47 +0800

    recent orders

--- frontend/src/business/components/listing/listingscript/contract/Contract.vue
+++ frontend/src/business/components/listing/listingscript/contract/Contract.vue
@@ -5,12 +5,10 @@
 </template>
 
 <script>
-import GoogleLogin from "@/business/components/listing/listingscript/common/components/GoogleLogin.vue";
-import SyncProdInfoDocs from "@/business/components/listing/listingscript/common/components/SyncProdDocs.vue";
 
 export default {
   name: "spot",
-  components: {SyncProdInfoDocs, GoogleLogin}
+  components: {},
 }
 </script>
 
