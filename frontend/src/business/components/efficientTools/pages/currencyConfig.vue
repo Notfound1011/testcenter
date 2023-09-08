2b9730b58a5a71c661dc4900645f6eedbdd8eb09
Pauri <pauri@cmexpro.com>
2023-09-08 10:09:39 +0800

    fix params

--- frontend/src/business/components/efficientTools/pages/currencyConfig.vue
+++ frontend/src/business/components/efficientTools/pages/currencyConfig.vue
@@ -85,7 +85,7 @@ export default {
     },
     async getCurrencyConfig () {
       const { data: currencyConfigRes } = await this.$axios.get(
-        `/pyServer/public/test-data/tools/get-currency-config?env=${this.form.env}&currency=BTC&getAll=true`,
+        `/pyServer/public/test-data/tools/get-currency-config?env=${this.formData.env}&currency=BTC&getAll=true`,
         // `naguri/ef_api/mock?env=${this.formData.env}&currency=BTC&getAll=true`,
       ).catch((error) => {
         commonOperator.messageTips('error', error)
