41b4cac2ce6e2f66efc7663602b56e23ce9a84a7
Pauri <pauri@cmexpro.com>
2023-09-11 11:08:30 +0800

    release 1.20.9

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker buildx build  --platform=linux/amd64 -t  3.1.250.199:5000/metersphere:v1.20.5 .
-docker push 3.1.250.199:5000/metersphere:v1.20.5
+docker buildx build  --platform=linux/amd64 -t  3.1.250.199:5000/metersphere:v1.20.9 .
+docker push 3.1.250.199:5000/metersphere:v1.20.9
