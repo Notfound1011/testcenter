724e02367f1b1ccc52f67a5bf1f00454f7eec27e
Pauri <pauri@cmexpro.com>
2023-11-08 11:48:27 +0800

    code coverage fixed.

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,3 @@
 npm run build
-docker buildx build  --platform=linux/amd64 -t  3.1.250.199:5000/frontend:v1.20.37 .
-docker push 3.1.250.199:5000/frontend:v1.20.37
+docker buildx build  --platform=linux/amd64 -t  3.1.250.199:5000/frontend:v1.20.38 .
+docker push 3.1.250.199:5000/frontend:v1.20.38
